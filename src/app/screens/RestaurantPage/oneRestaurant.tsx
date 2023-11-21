64c23cf53bd626386ca8cb006f19fd57790b21f5
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-11-21 20:36:48 +0900

    [62-63] feat: restaurant screen page allrestaurant component(fix)

--- src/app/screens/RestaurantPage/oneRestaurant.tsx
+++ src/app/screens/RestaurantPage/oneRestaurant.tsx
@@ -1,5 +1,12 @@
-import React from 'react'
+import { Container } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import SearchIcon from "@mui/icons-material/Search";
+import React from "react";
 
 export function OneRestaurant() {
-    return <div>OneRestaurant</div>;
+  return <div className="sinle_restaurant">
+    <Container>
+
+    </Container>
+  </div>;
 }
