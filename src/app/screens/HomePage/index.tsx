ccac4bb7abd815e08d3232244381b2e6f368f8e4
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-12-20 22:03:27 +0900

    [89-91] feat: usage of slice and selectors on homePage screen component

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,30 +1,54 @@
-import React, { useEffect } from 'react';
-import { Container } from "@mui/material";
-import { Statistics } from './statistics';
-import { TopRestaurant } from './topRestaurant';
-import { BestRestaurants } from './bestRestaurant';
-import { BestDishes } from './bestDishes';
-import { Advertisements } from './advertisements';
-import { Events } from './events';
-import { Recommendations } from './recommendations';
-import '../../../css/home.css';
+import React, { useEffect } from "react";
+import { Statistics } from "./statistics";
+import { TopRestaurant } from "./topRestaurant";
+import { BestRestaurants } from "./bestRestaurant";
+import { BestDishes } from "./bestDishes";
+import { Advertisements } from "./advertisements";
+import { Events } from "./events";
+import { Recommendations } from "./recommendations";
+import "../../../css/home.css";
+
+// REDUX
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setTopRestaurants } from "./slice";
+import { retrieveTopRestaurants } from "./selector";
+import { Restaurant } from "../../../types/user";
+
+/**REDUX SLICE */
+const actionDispatch = (dispach: Dispatch) => ({
+  setTopRestaurants: (data: Restaurant[]) => dispach(setTopRestaurants(data))
+});
+/**REDUX SELECTOR */
+const topRestaurantRetriever = createSelector(
+  retrieveTopRestaurants,
+  (topRestaurants) => ({
+    topRestaurants
+  })
+);
 
 export function HomePage() {
-useEffect(() => {
-    console.log('componentDidMount => Data fetch');
+  /**INITIALIZATION */
+  const { setTopRestaurants } = actionDispatch(useDispatch());
+  const { topRestaurants } = useSelector(topRestaurantRetriever);
 
-    return () => {
-      console.log("componentWillUnmount process");   
-    }
-}, []);
+  console.log("topRestaurants:::", topRestaurants);
 
-    return <div className='homepage'>
-        <Statistics/>
-        <TopRestaurant/>
-        <BestRestaurants/>
-        <BestDishes/>
-        <Advertisements/>
-        <Events/>
-        <Recommendations/>
+  useEffect(() => {
+    //backend data request => data
+    setTopRestaurants([]);
+  }, []);
+
+  return (
+    <div className="homepage">
+      <Statistics />
+      <TopRestaurant />
+      <BestRestaurants />
+      <BestDishes />
+      <Advertisements />
+      <Events />
+      <Recommendations />
     </div>
+  );
 }
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import HomePageReducer from './screens/HomePage/slice';
+import reduxLOgger from "redux-logger";
 
 export const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(reduxLOgger),
   reducer: {
     homePage: HomePageReducer
   },
