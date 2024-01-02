8c62f3834347a8d8bf8afc274986745e6bab13b4
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2024-01-02 22:31:28 +0900

    feat: basket component design publish

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -12,6 +12,8 @@ import {
 import Badge from "@mui/material/Badge";
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import Basket from "./basket";
+
 import {
   sweetTopSmallSuccessAlert,
   sweetTopSuccessAlert
@@ -71,20 +73,7 @@ export function NavbarHome(props: any) {
                 Yordam
               </NavLink>
             </Box>
-            <Box className="hover-line">
-              <IconButton
-                aria-label="cart"
-                id="basic-button"
-                aria-controls={undefined}
-                aria-haspopup="true"
-                aria-expanded={undefined}
-                //  onClick={handleClick}
-              >
-                <Badge badgeContent={3} color="secondary">
-                  <img src={"/icons/shopping_cart.svg"} alt="" />
-                </Badge>
-              </IconButton>
-            </Box>
+           <Basket/>
             {!props.verifiedMemberData ? (
               <Box>
                 <Button
